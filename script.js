bd25cc2df5925067730d5d97fb65509e3fa08d13
YuryPopeko <yurypopeko@icloud.com>
2018-04-01 20:54:09 +0300

    контакты, меню услуг
    вместо проверки страницы, проверка существования элемента

--- script.js
+++ script.js
@@ -10,11 +10,8 @@ document.querySelector('div.dark').addEventListener('click', function() {
 	closeMenu();
 });
 
-const servicesList = document.querySelector('.menu ul ul');
-
 document.addEventListener('keydown', function(event) {
-	if (event.keyCode === 27)
-		closeMenu();
+	if (event.keyCode === 27) closeMenu();
 });
 
 document.querySelector('.menu > ul > li > button').addEventListener('click', function() {
@@ -25,32 +22,33 @@ document.querySelector('.menu ul ul button').addEventListener('click', function(
 	document.querySelector('.menu ul ul').classList.remove('open');
 });
 
+const servicesList = document.querySelector('.menu ul ul');
+
 function closeMenu() {
 	document.body.classList.remove('menu');
-	if (servicesList.classList.contains('open'))
-		servicesList.classList.remove('open');
+	if (servicesList.classList.contains('open')) servicesList.classList.remove('open');
 }
 
 
 
 
-if (document.querySelector('section.salons')) {
-	document.querySelector('section.salons').addEventListener('click', function(event) {
-		if (event.target.classList.contains('modal'))
-			document.body.classList.add('modal');
-		});
+const callRequest = document.querySelector('section.salons button.modal');				// ЗАКАЗАТЬ ЗВОНОК
+if (callRequest) {
+	callRequest.addEventListener('click', function(event) {
+		document.body.classList.add('modal');
+	});
 
-		document.querySelector('.modal .close').addEventListener('click', function() {
-			document.body.classList.remove('modal');
-		});
+	document.querySelector('.modal .close').addEventListener('click', function() {
+		document.body.classList.remove('modal');
+	});
 
-		document.querySelector('div.dark').addEventListener('click', function() {
-			document.body.classList.remove('modal');
-		});
+	document.querySelector('div.dark').addEventListener('click', function() {
+		document.body.classList.remove('modal');
+	});
 
-		document.addEventListener('keydown', function(event) {
-			document.body.classList.remove('modal');
-		});
+	document.addEventListener('keydown', function(event) {
+		document.body.classList.remove('modal');
+	});
 }
 
 
@@ -103,28 +101,25 @@ function initMap() {																// MAP
 
 
 
-if (document.location.href.indexOf('service') > 0) {
-	document.querySelector('.salons').addEventListener('click', function(event) {
-		if (event.target.innerHTML === '360') {
-			const tourContainer = event.target.parentElement;
-			tourContainer.classList.add('open');
-			const close = document.querySelector('.tour.open .close');
-
-			close.addEventListener('click', function() {
-		        tourContainer.classList.remove('open');
-		    });
-		}
+const tour = document.querySelector('.tour div');										// 3D-TOUR
+if (tour) {
+	tour.addEventListener('click', function(event) {
+		const tourContainer = event.target.parentElement;
+		tourContainer.classList.add('open');
+		const close = document.querySelector('.tour.open .close');
+		close.addEventListener('click', function() {
+	        tourContainer.classList.remove('open');
+	    });
 	});
 }
 
 
 
 
-if (document.location.href.indexOf('gallery') > 0) {
-	const gallery = document.querySelector('main.gallery'),
-		  images = document.querySelectorAll('main.gallery > a');
-	document.querySelector('select').addEventListener('change', function(event) {
-		gallery.className = 'gallery clearfix';
-		gallery.classList.add(event.target.value);
+const toggleContent = document.querySelector('select.toggle-content');					// ЗАМЕНА КОНТЕНТА ПОД СЕЛЕКТОМ
+if (toggleContent) {
+	const main = document.querySelector('main');
+	toggleContent.addEventListener('change', function(event) {
+		main.dataset.content = event.target.value;
 	});
 }
